bf38234de9a6f3ecc9e3bbd255143ec2796d454c
dikshya2002 <ddikshya.agarwal2002@gmail.com>
2023-06-10 23:25:26 +0530

    [added css to the search option]

--- src/components/ProfileLists.jsx
+++ src/components/ProfileLists.jsx
@@ -8,6 +8,15 @@ const ProfileLists = () => {
     const [arr, setArr] = useState([]);
     const [searchStr, setSearchStr] = useState("");
     const [newarr, setNewArr] = useState([]);
+    const [wholearr, setWholeArr] = useState([]);
+    const [page, setPage] = useState(1);
+
+    const pageArr = [];
+    let i = 1;
+    while (i <= wholearr.length /8) {
+        pageArr.push(i);
+        i++;
+    }
 
     const handleChange = (e) => {
         setSearchStr(e.target.value);
@@ -21,7 +30,7 @@ const ProfileLists = () => {
     }, [searchStr]);
     //pagination....
 
-  
+
     // const pageArr = [];
     // let i = 1;
     // while (i <= newarr.length / 8) {
@@ -41,23 +50,33 @@ const ProfileLists = () => {
         })
             .then((res) => res.json())
             .then((data) => {
-               setArr(data);
+                setWholeArr(data);
+                const end = page *8;
+                const start = page - 1;
+                const newData = data.slice(start *8, end);
+                setArr(newData);
             });
     };
-
+    const handlePage = (el) => {
+        setPage(el);
+    };
     useEffect(() => {
         getData();
-      
-    }, [])
+
+    }, [page])
 
 
     return (
         <>
             <h1>Profiles</h1>
             <div className="indipro"></div>
-            <input type="text" name="search" id="search" onChange={(e) => handleChange(e)} value={searchStr} />
+            <div className="search">
+            <label>🔎</label>
+            <input type="text" name="search" id="search" placeholder="search by full name"onChange={(e) => handleChange(e)} value={searchStr} />
+            </div>
             {
                 newarr.length > 0 ? (
+                    <div>
                     <div className='Lists'>
 
                         {
@@ -79,30 +98,45 @@ const ProfileLists = () => {
 
 
                     </div>
-                ) : (searchStr.length == 0 ? (
-                    <div className='Lists'>
-                        {
-                            arr.map((e) => {
-                                return (
-                                    <ProfileCard
-                                        key={e.id}
-                                        id={e.id}
-                                        name={e.username}
-                                        followers={e.followers}
-                                        following={e.following}
-                                        bio={e.bio}
-                                        profile_picture={e.profile_picture}
-                                        verified={e.verified}
-                                    />
-                                )
-                            })
-                        }
-                    </div>
-                ) :
-                    <h1>No data found</h1>
+                     
+                       </div>
+
+
+                ) : (
+                    searchStr.length == 0 ? (
+                        <div className='Original'>
+                        <div className='Lists'>
+                            {
+                                arr.map((e) => {
+                                    return (
+                                        <ProfileCard
+                                            key={e.id}
+                                            id={e.id}
+                                            name={e.username}
+                                            followers={e.followers}
+                                            following={e.following}
+                                            bio={e.bio}
+                                            profile_picture={e.profile_picture}
+                                            verified={e.verified}
+                                        />
+                                    )
+                                })
+                            }
+                            </div>
+                            <div className='pageButtons'>
+                            {
+                                pageArr.map((el) => {
+                                    return <button onClick={() => handlePage(el)}>{el}</button>
+                                })
+                            }
+                            </div>
+                       </div>
+                    ) :
+                        (<h1>🦖No data found...</h1>)
                 )
             }
-          
+             
+
         </>
     );
 }
